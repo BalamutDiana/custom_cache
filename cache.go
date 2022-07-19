b09f8627f41abadc1053ca908ec2e04322dd1d14
BalamutDiana <balamutdiana@gmail.com>
2022-07-19 10:19:26 +0300

    update cache.go
    added error handling

--- cache.go
+++ cache.go
@@ -1,5 +1,9 @@
 package cache
 
+import (
+	"errors"
+)
+
 type Cache struct {
 	cache map[string]interface{}
 }
@@ -14,10 +18,24 @@ func (c Cache) Set(key string, value interface{}) {
 	c.cache[key] = value
 }
 
-func (c Cache) Get(key string) interface{} {
-	return c.cache[key]
+func (c Cache) keyExist(key string) bool {
+	_, exists := c.cache[key]
+	return exists
 }
 
-func (c Cache) Delete(key string) {
+func (c Cache) Get(key string) (interface{}, error) {
+	exists := c.keyExist(key)
+	if !exists {
+		return nil, errors.New("записи по ключу " + key + " не найдены")
+	}
+	return c.cache[key], nil
+}
+
+func (c Cache) Delete(key string) error {
+	exists := c.keyExist(key)
+	if !exists {
+		return errors.New("записи по ключу " + key + " не найдены")
+	}
 	delete(c.cache, key)
+	return nil
 }
